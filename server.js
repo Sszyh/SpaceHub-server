8bcbaaaef4f50cfef7d6629f7ff776dd7c7b1260
Sszyh <zhuyer@outlook.com>
2023-01-25 11:57:56 -0800

    config morgan and nodemon

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const cors = require('cors');
 
 const app = express();
 const PORT = 8000;
+const morgan = require('morgan');
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.get('/message', (req, res) => {
   res.json({ message: "Hello from server!" });
