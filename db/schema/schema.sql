f70c3a6fb6bb29fed8e48b8d0918f8a454ad4741
Ruanruanx <zxin7@uwo.ca>
2023-01-26 03:02:13 -0500

    Added seeds for database

--- db/schema/schema.sql
+++ db/schema/schema.sql
@@ -49,7 +49,7 @@ CREATE TABLE bookings (
   check_in_date DATE NOT NULL,
   check_out_date DATE NOT NULL,
   price_for_stay MONEY NOT NULL,
-  
+  price_per_day MONEY NOT NULL,
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL
 );
